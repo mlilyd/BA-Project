b9265b67898cc872e8d645f98337258d6ba8f1c7
Lily D <lily@stud.uni-heidelberg.de>
2020-12-07 17:43:14 +0100

    Testing OpenCL

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,10 +1,17 @@
 #include "../Header/Renderer.h"
 #include "../Header/Camera.h"
 
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
 #include <iostream>
 #include <CL/cl.h>
 #include <CL/opencl.h>
 
+#define ARRAY_SIZE 64
 
 //OpenCL accelerations
 
@@ -28,7 +35,7 @@ cl_device_id create_device() {
     /* Identify a platform */
     err = clGetPlatformIDs(1, &platform, NULL);
     if (err < 0) {
-        perror("Couldn't identify a platform");
+        std::cout<<"Couldn't identify a platform";
         exit(1);
     }
 
@@ -40,7 +47,7 @@ cl_device_id create_device() {
         err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_CPU, 1, &dev, NULL);
     }
     if (err < 0) {
-        perror("Couldn't access any devices");
+        std::cout << "Couldn't access any devices";
         exit(1);
     }
 
@@ -48,28 +55,17 @@ cl_device_id create_device() {
 }
 
 
-/* Create program from a file and compile it */
-cl_program build_program(cl_context ctx, cl_device_id dev, const char* filename) {
+/* Create program from a std::string and compile it */
+cl_program build_program(cl_context ctx, cl_device_id device, std::string kernel_code) {
 
     cl_program program;
-    FILE* program_handle;
-    char* program_buffer, * program_log;
-    size_t program_size, log_size;
+    char* program_log;
+    size_t log_size;
     int err;
 
-    /* Read program file and place content into buffer */
-    program_handle = fopen(filename, "r");
-    if (program_handle == NULL) {
-        perror("Couldn't find the program file");
-        exit(1);
-    }
-    fseek(program_handle, 0, SEEK_END);
-    program_size = ftell(program_handle);
-    rewind(program_handle);
-    program_buffer = (char*)malloc(program_size + 1);
-    program_buffer[program_size] = '\0';
-    fread(program_buffer, sizeof(char), program_size, program_handle);
-    fclose(program_handle);
+    const size_t program_size = kernel_code.length();
+    const char* kernel_code_c = kernel_code.c_str();
+    
 
     /* Create program from file
 
@@ -77,13 +73,11 @@ cl_program build_program(cl_context ctx, cl_device_id dev, const char* filename)
     Specifically, the code reads the file's content into a char array
     called program_buffer, and then calls clCreateProgramWithSource.
     */
-    program = clCreateProgramWithSource(ctx, 1,
-        (const char**)&program_buffer, &program_size, &err);
+    program = clCreateProgramWithSource(ctx, 1, &kernel_code_c, &program_size, &err);
     if (err < 0) {
         perror("Couldn't create the program");
         exit(1);
     }
-    free(program_buffer);
 
     /* Build program
 
@@ -96,11 +90,11 @@ cl_program build_program(cl_context ctx, cl_device_id dev, const char* filename)
     if (err < 0) {
 
         /* Find size of log and print to std output */
-        clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG,
+        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG,
             0, NULL, &log_size);
         program_log = (char*)malloc(log_size + 1);
         program_log[log_size] = '\0';
-        clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG,
+        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG,
             log_size + 1, program_log, NULL);
         printf("%s\n", program_log);
         free(program_log);
@@ -322,13 +316,163 @@ int main(){
     cl_int i, j, err;
     size_t local_size, global_size;
 
+    /* Data and buffers    */
+    float data[ARRAY_SIZE];
+    float sum[2], total, actual_sum;
+    cl_mem input_buffer, sum_buffer;
+    cl_int num_groups;
 
-
-    const int width = 500;
-    const auto aspect_ratio = 16.0/9.0;
-    const int height = width/aspect_ratio;
-    const int samples = 200;
+    /* Initialize data */
+    for (i = 0; i < ARRAY_SIZE; i++) {
+        data[i] = 1.0f * i;
+    }
+   
+    /* Create device and context
     
+
+   Creates a context containing only one device — the device structure
+   created earlier.
+   */
+    device = create_device();
+    context = clCreateContext(NULL, 1, &device, NULL, NULL, &err);
+    if (err < 0) {
+        perror("Couldn't create a context");
+        exit(1);
+    }
+
+    //define kernel function as string
+    std::string kernel_code = 
+        "   void kernel simple_add(global const int* A, global const int* B, global int* C, "
+        "                          global const int* N) {"
+        "       int ID, Nthreads, n, ratio, start, stop;"
+        ""
+        "       ID = get_global_id(0);"
+        "       Nthreads = get_global_size(0);"
+        "       n = N[0];"
+        ""
+        "       ratio = (n / Nthreads);"  // number of elements for each thread
+        "       start = ratio * ID;"
+        "       stop  = ratio * (ID + 1);"
+        ""
+        "       for (int i=start; i<stop; i++)"
+        "           C[i] = A[i] + B[i];"
+        "   }";
+
+    /* Build program */
+    program = build_program(context, device, kernel_code);
+    
+    /* Create data buffer
+
+    • `global_size`: total number of work items that will be
+       executed on the GPU (e.g. total size of your array)
+    • `local_size`: size of local workgroup. Each workgroup contains
+       several work items and goes to a compute unit
+
+    In this example, the kernel is executed by eight work-items divided into
+    two work-groups of four work-items each. Returning to my analogy,
+    this corresponds to a school containing eight students divided into
+    two classrooms of four students each.
+
+      Notes:
+    • Intel recommends workgroup size of 64-128. Often 128 is minimum to
+    get good performance on GPU
+    • On NVIDIA Fermi, workgroup size must be at least 192 for full
+    utilization of cores
+    • Optimal workgroup size differs across applications
+    */
+    global_size = 8; // WHY ONLY 8?
+    local_size = 4;
+    num_groups = global_size / local_size;
+    input_buffer = clCreateBuffer(context, CL_MEM_READ_ONLY |
+        CL_MEM_COPY_HOST_PTR, ARRAY_SIZE * sizeof(float), data, &err); // <=====INPUT
+    sum_buffer = clCreateBuffer(context, CL_MEM_READ_WRITE |
+        CL_MEM_COPY_HOST_PTR, num_groups * sizeof(float), sum, &err); // <=====OUTPUT
+    if (err < 0) {
+        perror("Couldn't create a buffer");
+        exit(1);
+    };
+
+    /* Create a command queue
+
+    Does not support profiling or out-of-order-execution
+    */
+    queue = clCreateCommandQueue(context, device, 0, &err);
+    if (err < 0) {
+        perror("Couldn't create a command queue");
+        exit(1);
+    };
+
+    /* Create a kernel */
+    kernel = clCreateKernel(program, "simple_add", &err);
+    if (err < 0) {
+        perror("Couldn't create a kernel");
+        exit(1);
+    };
+
+    /* Create kernel arguments */
+    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &input_buffer); // <=====INPUT
+    err = clSetKernelArg(kernel, 1, local_size * sizeof(float), NULL);
+    err = clSetKernelArg(kernel, 2, sizeof(cl_mem), &sum_buffer); // <=====OUTPUT
+    if (err < 0) {
+        perror("Couldn't create a kernel argument");
+        exit(1);
+    }
+
+    /* Enqueue kernel
+
+    At this point, the application has created all the data structures
+    (device, kernel, program, command queue, and context) needed by an
+    OpenCL host application. Now, it deploys the kernel to a device.
+
+    Of the OpenCL functions that run on the host, clEnqueueNDRangeKernel
+    is probably the most important to understand. Not only does it deploy
+    kernels to devices, it also identifies how many work-items should
+    be generated to execute the kernel (global_size) and the number of
+    work-items in each work-group (local_size).
+    */
+    err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &global_size,
+        &local_size, 0, NULL, NULL);
+    if (err < 0) {
+        perror("Couldn't enqueue the kernel");
+        exit(1);
+    }
+
+    /* Read the kernel's output    */
+    err = clEnqueueReadBuffer(queue, sum_buffer, CL_TRUE, 0,
+        sizeof(sum), sum, 0, NULL, NULL); // <=====GET OUTPUT
+    if (err < 0) {
+        perror("Couldn't read the buffer");
+        exit(1);
+    }
+
+    /* Check result */
+    total = 0.0f;
+    for (j = 0; j < num_groups; j++) {
+        total += sum[j];
+    }
+    actual_sum = 1.0f * ARRAY_SIZE / 2 * (ARRAY_SIZE - 1);
+    printf("Computed sum = %.1f.\n", total);
+    if (fabs(total - actual_sum) > 0.01 * fabs(actual_sum))
+        printf("Check failed.\n");
+    else
+        printf("Check passed.\n");
+
+    /* Deallocate resources */
+    clReleaseKernel(kernel);
+    clReleaseMemObject(sum_buffer);
+    clReleaseMemObject(input_buffer);
+    clReleaseCommandQueue(queue);
+    clReleaseProgram(program);
+    clReleaseContext(context);
+    return 0;
+
+
+    /*
+    const int width = 500;
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int height = width / aspect_ratio;
+    const int samples = 200;
+
     const int max_depth = 5;
     const int ray_shot = 2;
     //Camera camera(Point3(-100, 500,-1000), Point3(300, 300, 0), vec3(0,1,0), 45, aspect_ratio);
@@ -341,7 +485,5 @@ int main(){
     //vector<Color> img = render(camera, scene, background, width, height, samples, max_depth);
     //vector<Color> img_ao = render_AO(camera, scene, background, width, height, samples, max_depth, ray_shot);
 
-    //write_img_jpg(img, "renders/test_11.jpg", width, height, samples);
-
-
+    //write_img_jpg(img, "renders/test_11.jpg", width, height, samples);*/
 }
